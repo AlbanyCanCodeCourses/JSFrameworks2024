6be70310450511dbcf73316c62c2bf514897f2f4
jstella414 <jstella414@gmail.com>
2024-06-05 19:58:34 -0400

    finished review questions

--- exercises/00-review/00-review.js
+++ exercises/00-review/00-review.js
@@ -1,3 +1,4 @@
+import greet from "./greet.js";
 /**
  * Solve this problem using ES modules (ESM).
  * ES modules allow you break up your code into multiple files (or modules), and then share code between different files.
@@ -33,6 +34,14 @@ const greet = (name) => {
 
 const highestNumber = (array) => {
   // Write your answer here
+let highest = array[0]
+
+  array.forEach(num => {
+    if(num > highest){
+      highest = num
+    }
+  });
+  return highest
 };
 
 /**
@@ -46,6 +55,9 @@ const highestNumber = (array) => {
 
 const combineArray = (array1, array2) => {
   // Write your answer here
+
+  const newArray = [...array1, ...array2]
+  return newArray
 };
 
 /**
@@ -57,6 +69,9 @@ const combineArray = (array1, array2) => {
 
 const combineObject = (obj1, obj2) => {
   // Write your answer here
+  const myObject = {...obj1,...obj2}
+
+  return myObject
 };
 
 /**
@@ -68,6 +83,7 @@ const combineObject = (obj1, obj2) => {
 
 const doubleValues = (arr) => {
   // Write your answer here
+  return arr.map(num => num * 2);
 };
 
 /**
@@ -80,7 +96,14 @@ const doubleValues = (arr) => {
  *   onlyEvenValues([5,1,2,3,10]) // [2,10]
  */
 const onlyEvenValues = (arr) => {
-  // Write your answer here
+  let newArray = []
+  arr.forEach(num =>{
+    if (num % 2 == 0){
+      newArray.push(num)
+    }
+    // Write your answer here
+  })
+return newArray 
 };
 
 /**
@@ -102,7 +125,13 @@ const onlyEvenValues = (arr) => {
  *  removeVowels('ZZZZZZ') // ('zzzzzz')
  */
 const removeVowels = (str) => {
-  // Write your answer here
+  let newstring = '';
+  
+  str.toLowerCase().split("").forEach(letter =>{
+    if(!'aeiouAEIOU'.includes(letter))
+      newstring += letter
+    })
+    return newstring
 };
 
 /**
@@ -122,10 +151,9 @@ const getIsHungryText = () => {
   // } else {
   //   isHungry = "Keep coding!";
   // }
-
+  let isHungry = isStomachEmpty ? "Go eat something." : "Keep coding!" ;
   return isHungry;
 };
-
 /**
  * Refactor this to use destructuring. For an example of destructuring,
  * @see https://dmitripavlutin.com/javascript-object-destructuring/
@@ -140,10 +168,15 @@ const getTempOfTomorrow = () => {
   };
 
   // Start of what you should change
-  const today = AVG_TEMPERATURES.today;
-  const tomorrow = AVG_TEMPERATURES.tomorrow;
+  // const today = AVG_TEMPERATURES.today;
+  // const tomorrow = AVG_TEMPERATURES.tomorrow;
+
+  const { today, tomorrow } = AVG_TEMPERATURES;
+
   // End of what you should change
   return `Today's temperature is ${today}.\nTomorrow's temperature is ${tomorrow}`;
+
+  
 };
 
 /**
@@ -158,6 +191,8 @@ const getTempOfTomorrow = () => {
  *  addItems([1,-2,-3]) // -4
  */
 const addItems = (arr) => {
+
+  return arr.reduce((sum, num) => sum + num);
   // Write your answer here
 };
 
@@ -175,6 +210,8 @@ const addItems = (arr) => {
 const removeDuplicates = (array) => {
   // Write your answer here
   // Return an array of unique values
+  return [...new Set(array)];
+
 };
 
 /**

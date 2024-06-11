57a2725cefbe02cc7d1e0377d75bb39d48f02007
Noe_G <noe.gonzalezortiz@gmail.com>
2024-06-10 20:37:47 -0400

    new

--- exercises/02-collections/src/App.jsx
+++ exercises/02-collections/src/App.jsx
@@ -1,8 +1,21 @@
 import "./App.css";
 // import something here
+import {technologies} from './images/technologies';
+import {phones} from './images/phones';
 
 function App() {
-  return <div className="App">{/* Complete me */}</div>;
+  return <div className="App">
+    <div>
+      {technologies.map((technologies,index)
+       => {
+      const key = 'index-${index}';
+      return <img src={technologies}
+      width= "200" key={key} />;
+    })} 
+    </div>
+    
+  
+  </div>;
 }
 
 export default App;

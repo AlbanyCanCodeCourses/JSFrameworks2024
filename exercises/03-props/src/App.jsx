bb6f28bc46832df0b73d6bb51f6fc2dd38fc5c70
Christopher Druke <cdrukepike@gmail.com>
2024-06-12 18:35:19 -0400

    Implement solutions for exercises 01/02/03

--- exercises/03-props/src/App.jsx
+++ exercises/03-props/src/App.jsx
@@ -1,8 +1,14 @@
 import "./App.css";
-// import something here
+import Welcome from "./Welcome";
 
 function App() {
-  return <div className="App">{/* Complete me */}</div>;
+  return (
+    <div className="App">
+      <h1>Welcome to my React App!</h1>
+      <Welcome name="Chris" />
+      <Welcome />
+    </div>
+  );
 }
 
 export default App;
---
+++ exercises/03-props/src/Welcome.jsx
@@ -0,0 +1,3 @@
+export default function Welcome({ name = "user" }) {
+  return <p name={name}>Hello {name}</p>;
+}

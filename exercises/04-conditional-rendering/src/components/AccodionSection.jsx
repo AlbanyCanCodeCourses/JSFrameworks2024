b7e116e1d88ca46da707795b64982990b5fe9830
Noe_G <noe.gonzalezortiz@gmail.com>
2024-06-15 12:05:13 -0400

    update

---
+++ exercises/04-conditional-rendering/src/components/AccodionSection.jsx
@@ -0,0 +1,18 @@
+function AccordionSection(){
+return <>
+    <div class="title active">
+      <i class="dropdown icon"></i>
+      Why would anyone use React?
+    </div>
+    <div class="content active">
+      <p class="transition visible">
+        One of the main benefits of using React JS is its potential to reuse
+        components. It saves time for developers as they don't have to write various
+        codes for the same features. Furthermore, if any changes are made in any
+        particular part, it will not affect other parts of the application.
+      </p>
+    </div>
+
+</>
+}
+export default AccordionSection

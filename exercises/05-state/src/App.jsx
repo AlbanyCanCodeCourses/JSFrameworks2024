b2dbbcba2a05293020381fd788fd2ff6878d3e7b
Christopher Druke <cdrukepike@gmail.com>
2024-06-17 21:01:54 -0400

    Implement exercise05 solutions

--- exercises/05-state/src/App.jsx
+++ exercises/05-state/src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import DiceRoller from "./components/DiceRoll/DiceRoll";
-// Import LessText here
+import LessText from "./components/LessText/LessText";
 
 function App() {
+  const sampleLoremText = "Praesent metus diam, laoreet non quam eu, luctus maximus ante. Donec lorem nisl, fermentum ac eros in, feugiat lacinia est. Phasellus dictum nunc eget ligula euismod efficitur.";
   return (
     <>
       <div className="container pt-4 pb-4">
         <h1 className="h3">Challenge 1 - Less Text</h1>
-        {/* Add LessText with props here */}
+        <LessText text={sampleLoremText} maxLength={30} />
       </div>
       <div className="pt-4 pb-4 mb-4">
         <div className="container">

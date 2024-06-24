3576f2b1f1d5391499bf4c5489b7a326624f250f
megpen97 <megpen97@yahoo.com>
2024-06-24 18:07:35 -0400

    DiceRoll

--- exercises/05-state/src/components/DiceRoll/DiceRoll.jsx
+++ exercises/05-state/src/components/DiceRoll/DiceRoll.jsx
@@ -4,6 +4,7 @@ import dice3 from "./dice3.svg";
 import dice4 from "./dice4.svg";
 import dice5 from "./dice5.svg";
 import dice6 from "./dice6.svg";
+import { useState } from "react";
 
 const images = { dice1, dice2, dice3, dice4, dice5, dice6 };
 

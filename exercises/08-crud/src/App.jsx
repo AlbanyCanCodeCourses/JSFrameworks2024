964fd07bb0301aa88d25c5323b42743c340c4688
megpen97 <megpen97@yahoo.com>
2024-06-27 09:00:04 -0400

    Grocery List

--- exercises/08-crud/src/App.jsx
+++ exercises/08-crud/src/App.jsx
@@ -1,7 +1,39 @@
-// Import something here
+import { useState } from "react";
+import React from "react";
 import "./App.css";
 
+
 const GroceryList = () => {
+  const [groceryName, setGroceryName ] = useState("")
+  const [groceryCost, setGroceryCost] = useState(Number(0));
+
+  const [list, setList] = useState([
+    { groceryName: "Bananas", cost: 5.99 },
+    { groceryName: "Apples", cost: 2.99 }
+  ]);
+
+  const addItem = (item) => {
+    const newItem = { groceryName: groceryName, cost: groceryCost };
+    setList([...list, newItem]);
+    newItem.cost = parseInt(newItem.cost);
+    setTotal(sum(totalCost + newItem.cost));
+  }
+
+  const totalCost = list.reduce((totalCost, grocery) => totalCost + grocery.cost, 0);
+
+  const clearList = () => {
+    setList([]);
+    setTotal(0);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    addItem();
+    
+  };
+
+
   return (
     <div className="container">
       <div className="card card-body bg-light mb-2">
@@ -10,6 +42,8 @@ const GroceryList = () => {
             <input
               className="form-control"
               type="text"
+              value={groceryName}
+              onChange={(e) => setGroceryName(e.target.value)}
               placeholder="Name of grocery item..."
               aria-label="Name of grocery item..."
             />
@@ -20,12 +54,15 @@ const GroceryList = () => {
               type="number"
               min="0"
               step=".01"
+              value={groceryCost}
+              onChange={(e) => setGroceryCost(e.target.value)}
               placeholder="Cost of grocery item..."
               aria-label="Cost of grocery item..."
             />
           </div>
           <div className="col-md-auto">
-            <button type="submit" className="btn btn-success">
+            <button type="submit"className="btn btn-success" 
+            onClick={(e) => handleSubmit(e)}>
               Add
             </button>
           </div>
@@ -42,26 +79,31 @@ const GroceryList = () => {
             </tr>
           </thead>
           <tbody>
-            {/**
-             * Complete me. (You can use something else instead of a table if you like)
-             * @example
-             * <tr>
-             *   <td>Toilet Paper</td>
-             *   <td>$1.99</td>
-             *   <td>
-             *     <button aria-label="Delete" title="Delete" ... >
-             *       &times;
-             *     </button>
-             *   </td>
-             * </tr>
-             */}
+            {
+              list.map((item) => (
+                <tr key={item.groceryName}>
+                  <td>{item.groceryName}</td>
+                  <td>${item.cost}</td>
+                  <td>
+                    <button
+                      type="button"
+                      className="btn btn-outline-danger"
+                      // set to remove item clicked from list
+                      onClick={() => setList(list.filter((grocery) => grocery.groceryName !== item.groceryName))}
+                    >
+                      Remove
+                    </button>
+                  </td>
+                </tr>
+              ))
+            }
           </tbody>
         </table>
         <p className="lead">
-          <strong>Total Cost: {/* Complete me */}</strong>
+          <strong>Total Cost: ${totalCost}</strong>
         </p>
         <div className="d-flex justify-content-end">
-          <button type="button" className="btn btn-outline-success">
+          <button type="button" onClick={clearList} className="btn btn-outline-success">
             Clear
           </button>
         </div>

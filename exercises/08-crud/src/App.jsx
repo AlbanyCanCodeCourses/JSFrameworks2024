b3a13d851d0be0e00a20aae344b56258063d99b8
jstella414 <jstella414@gmail.com>
2024-07-08 14:07:43 -0400

    finished CRUD only need to figure out the Total cost function

--- exercises/08-crud/src/App.jsx
+++ exercises/08-crud/src/App.jsx
@@ -1,28 +1,90 @@
 // Import something here
 import "./App.css";
+import {useState} from "react";
 
 const GroceryList = () => {
+const [groceryName, setGroceryName] = useState("");
+const [cost, setCost] = useState("");
+const [hasError, setHasError] = useState("")
+  const [list, setList] = useState([
+    {groceryName: "Bananas", cost:5.99},
+    {groceryName: "Apples", cost:1.33}
+   ]);
+
+   const addItem = () =>{
+     const newItem = {
+       groceryName: groceryName,
+       cost: parseFloat(cost)
+      };
+      setList([...list, newItem]);
+      
+   };
+   
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    if (groceryName &&(cost || cost === 0)){
+      addItem();
+      setHasError(false);
+    }else setHasError(true);
+   };
+  
+  const deleteItem = (index)=>{
+    setList(
+      list.filter((_, currentIndex) => currentIndex !== index)
+    )
+  }
+
+
+const findTotal = () =>{
+ list.reduce((total, item)=>{
+
+ console.log(total + item.cost)
+ }, 0);
+}
+
+
+  const handleClear = ()=>{
+    setList([]);
+
+  }
+
   return (
     <div className="container">
       <div className="card card-body bg-light mb-2">
-        <form method="POST" className="row g-3">
+        <form method="POST" className="row g-3" onSubmit={handleSubmit}>
           <div className="col">
             <input
-              className="form-control"
+             
+              className = {hasError && !groceryName ? "is-invalid form-control" : "form-control"}
               type="text"
               placeholder="Name of grocery item..."
               aria-label="Name of grocery item..."
+              value = {groceryName}
+              onChange = {(e) => setGroceryName(e.target.value)}
             />
+
+            {hasError && !groceryName && (
+            <div className="invalid-feedback">
+                Please enter a grocery item
+              </div>
+            )}
           </div>
           <div className="col">
             <input
-              className="form-control"
+              className = {hasError && !groceryName ? "is-invalid form-control" : "form-control"}
               type="number"
               min="0"
               step=".01"
               placeholder="Cost of grocery item..."
               aria-label="Cost of grocery item..."
+              value = {cost}
+              onChange = {(e)=>setCost(e.target.value)}
             />
+            {hasError && !cost && (
+              <div className="invalid-feedback">
+              Please enter a cost
+            </div>
+            )}
           </div>
           <div className="col-md-auto">
             <button type="submit" className="btn btn-success">
@@ -55,13 +117,30 @@ const GroceryList = () => {
              *   </td>
              * </tr>
              */}
+             {list.map((item, index)=>{
+              
+              return(
+                <>
+                <tr>
+                   <td>{item.groceryName}</td>
+                   <td>{item.cost.toFixed(2)}</td>
+                   <td>
+                     <button aria-label="Delete" title="Delete" onClick={() => deleteItem(index)} >
+                       &times;
+                     </button>
+                   </td>
+                 </tr>
+                 </>
+              )
+
+             })}
           </tbody>
         </table>
         <p className="lead">
-          <strong>Total Cost: {/* Complete me */}</strong>
+          <strong>Total Cost: ${findTotal()}</strong>
         </p>
         <div className="d-flex justify-content-end">
-          <button type="button" className="btn btn-outline-success">
+          <button type="button" className="btn btn-outline-success" onClick={handleClear}>
             Clear
           </button>
         </div>
